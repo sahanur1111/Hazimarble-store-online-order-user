3e286aa6079a78fe52b66ad1faf95479abd36cb9
sahanur1111 <118846504+sahanur1111@users.noreply.github.com>
2024-03-29 18:40:17 +0530

    ok

--- src/contact_us/Contact.jsx
+++ src/contact_us/Contact.jsx
@@ -34,19 +34,99 @@ const Contact = () => {
           {/* <h2 className='text-red'>Hazi Marble House</h2><hr/> */}
           Lohapur Kantagoriya, Nalhati, Birbhum, Pin-731237
         </address>
-        <div
-          className="relative overflow-hidden rounded-md"
-          style={{ paddingBottom: "40%" }}
-        >
-          <iframe
-           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3636.6056932735937!2d87.96393907355808!3d24.29049926783084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fa250dce31d54b%3A0x33e1098caeaa3b0d!2sHazi%20Marble%20House%20Godown!5e0!3m2!1sen!2sin!4v1708127944428!5m2!1sen!2sin"
-            frameBorder="0"
-            allowFullScreen=""
-            aria-hidden="false"
-            tabIndex="0"
-            className="absolute inset-0 w-full h-full"
-          ></iframe>
-        </div>
+        <div className="w-full md:w-[870px] mx-auto px-4">
+      <h2 className="text-2xl my-4 font-bold">
+        Upload A New <span className="text-green ">Product</span>
+      </h2>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text font-semibold">Product Name<span className="text-red">*</span></span>
+            </label>
+            <input
+              type="text"
+              placeholder="Product Name"
+              {...register("name", { required: true })}
+              required
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="flex gap-6">
+            {/* category */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text font-semibold">Category<span className="text-red">*</span></span>
+              </label>
+              <select
+                defaultValue="default"
+                {...register("category", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled value="default">
+                  Select a category
+                </option>
+                <option value="marble">Marble</option>
+                <option value="wall">Wall Tiles</option>
+                <option value="floor">Floor Tiles</option>
+                <option value="plumbing">Plumbing</option>
+                <option value="sanitary">Sanitary</option>
+                <option value="popular">Popular</option>
+                <option value="offer">Offer</option>
+              </select>
+            </div>
+
+            {/* price */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text font-semibold">Price<span className="text-red">*</span></span>
+              </label>
+              <input
+                type="number"
+                placeholder="Price"
+                {...register("price", { required: true })}
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+           {/* optional */}
+           <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text font-semibold">Only for Special</span>
+            </label>
+            <input
+              type="text"
+              {...register("optional", { required: true })}
+              placeholder="2 Special word"
+              className="input input-bordered w-full"
+            />
+          </div>
+          {/* product details */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold">Product Details</span>
+            </label>
+            <textarea
+              {...register("product", { required: true })}
+              className="textarea textarea-bordered h-24"
+              placeholder="Type some word about your product up to 20 words."
+            ></textarea>
+          </div>
+             {/* 4th row */}
+          <div className="form-control w-full my-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+
+          <button className="btn bg-green text-white px-6">
+            Add Item <AiTwotoneFileAdd/>
+          </button>
+        </form>
+      </div>
+    </div>
     </div>
   );
 };
