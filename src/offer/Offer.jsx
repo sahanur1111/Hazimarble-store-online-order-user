edf38624d51175bc7fb5c081f1ffc622cf91543b
sahanur1111 <118846504+sahanur1111@users.noreply.github.com>
2024-04-02 04:23:33 +0530

    new
    added

--- src/offer/Offer.jsx
+++ src/offer/Offer.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../public/images/home/banner.png";
+import img2 from "../../public/images/home/banner2.png";
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div>
+      <Carousel
+        infiniteLoop
+        autoPlay
+        showStatus={false}
+        showArrows={false}
+        showThumbs={false}
+        interval={2000}
+      >
+        <div>
+          <img src={img1} alt="Item1" />
+          <p className="legend">Full Stack</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+      </Carousel>
+    </div>
   )
 }
 
